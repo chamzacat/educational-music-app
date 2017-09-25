5691c88a0b7f46d3658af4e36d31dfe183ffb0de
Bancroft <chamza@aapl000621.local>
2017-09-25 09:45:03 -0400

    Initial Commit

---
+++ educational music/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  educational music
+//
+//  Created by Celine Hamza on 9/25/17.
+//  Copyright © 2017 Celine Hamza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
